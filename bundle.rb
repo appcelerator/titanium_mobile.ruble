3cc50f57437a4b42677e2a07db6511d55ce0ef10
Ingo Muschenetz <ingo@muschenetz.com>
2011-11-11 09:26:50 -0800

    Merged in @kwhinnery pull request https://github.com/appcelerator/titanium_mobile.ruble/pull/1

--- bundle.rb
+++ bundle.rb
@@ -11,13 +11,32 @@ bundle 'Appcelerator Titanium Mobile' do |bundle|
 
   bundle.menu 'Titanium Mobile' do |main_menu|
     main_menu.command 'Titanium Mobile Developer Center'
-    main_menu.command 'Xcode Debug' if Ruble.is_mac?
-
     main_menu.command 'Ti.include()'
     main_menu.command 'Ti.API.debug'
     main_menu.command 'Ti.API.info'
     main_menu.command 'Ti.API.log'
     main_menu.command 'Ti.App.fireEvent'
     main_menu.command 'Ti.App.addEventListener'
+  end
+  
+  project_template "Single Window Application" do |t|
+    t.type = :titanium_mobile
+    t.location = "staging/SingleWindow.zip"
+    t.description = "A single window application with a single view - a mostly blank canvas."
+    puts t
+  end
+  
+  project_template "Master/Detail Application" do |t|
+    t.type = :titanium_mobile
+    t.location = "staging/MasterDetail.zip"
+    t.description = "A master detail view, utilizing a native table view component and platform-specific UI and navigation.  A starting point for a navigation-based application with hierarchical data, or a stack of windows."
+    puts t
+  end
+  
+  project_template "Tabbed Application" do |t|
+    t.type = :titanium_mobile
+    t.location = "staging/Tabbed.zip"
+    t.description = "A tabbed application, consisting of multiple stacks of windows associated with tabs in a tab group.  A starting point for tab-based application with multiple top-level windows."
+    puts t
   end
 end
