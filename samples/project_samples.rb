ca3268942075814df9387428091c4e934fc56ae4
Ingo Muschenetz <ingo@muschenetz.com>
2011-12-07 08:29:17 -0800

    Updated sample/template icons.

--- samples/project_samples.rb
+++ samples/project_samples.rb
@@ -6,6 +6,7 @@ require 'ruble'
   # s.location = "git://github.com/appcelerator-developer-relations/Sample.Mapping.git"
   # s.description = "A sample Master/Detail app that uses native maps to plot locations. With it you can forward geocode addresses and add them as annotations to the map."
   # s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
+  # s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_geo.png"
 # end
 # 
 # project_sample "RSS Reader" do |s|
@@ -14,6 +15,7 @@ require 'ruble'
   # s.location = "git://github.com/appcelerator-developer-relations/Sample.RSS.git"
   # s.description = "A sample Master/Detail app that creates a RSS reader. With it you can pull live RSS feeds from the internet, list them along with thumbnails, then drill down to the article itself."
   # s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
+  # s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_rss.png"
 # end
 # 
 # project_sample "Todo List" do |s|
@@ -22,6 +24,7 @@ require 'ruble'
   # s.location = "git://github.com/appcelerator-developer-relations/Sample.Todo.git"
   # s.description = "A sample tabbed application that creates a basic todo list. With this app you can maintain a listing of tasks to be completed, add to that list, and mark tasks as done."
   # s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
+  # s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_to_do_list.png"
 # end
 
 project_sample "Kitchen Sink" do |s|
@@ -30,6 +33,7 @@ project_sample "Kitchen Sink" do |s|
   s.location = "git://github.com/appcelerator/KitchenSink.git"
   s.description = "A sample Titanium Mobile project that demonstrates a variety of APIs available in Titanium Mobile"
   s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
+  s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_kitchen_sink.png"
 end
 
 project_sample "Kitchen Sink iPad" do |s|
@@ -38,5 +42,6 @@ project_sample "Kitchen Sink iPad" do |s|
   s.location = "git://github.com/appcelerator/KitchenSinkiPad.git"
   s.description = "A sample Titanium Mobile project that demonstrates a variety of APIs available in Titanium Mobile (For iPad)"
   s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
+  s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_kitchen_sink.png"
 end
 
