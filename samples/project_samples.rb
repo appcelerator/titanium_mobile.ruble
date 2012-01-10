a78cc44288eb5b2e970ce567f114f25e1ebf9586
Kevin Whinnery <kevin.whinnery@gmail.com>
2012-01-10 12:51:21 -0800

    new templates

--- samples/project_samples.rb
+++ samples/project_samples.rb
@@ -1,31 +1,31 @@
 require 'ruble'
 
-# project_sample "Geocoder" do |s|
-  # s.id = "com.appcelerator.titanium.mobile.samples.mapping"
-  # s.category = "com.appcelerator.titanium.mobile.samples.category"
-  # s.location = "git://github.com/appcelerator-developer-relations/Sample.Mapping.git"
-  # s.description = "A sample Master/Detail app that uses native maps to plot locations. With it you can forward geocode addresses and add them as annotations to the map."
-  # s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
-  # s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_geo.png"
-# end
-# 
-# project_sample "RSS Reader" do |s|
-  # s.id = "com.appcelerator.titanium.mobile.samples.rss"
-  # s.category = "com.appcelerator.titanium.mobile.samples.category"
-  # s.location = "git://github.com/appcelerator-developer-relations/Sample.RSS.git"
-  # s.description = "A sample Master/Detail app that creates a RSS reader. With it you can pull live RSS feeds from the internet, list them along with thumbnails, then drill down to the article itself."
-  # s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
-  # s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_rss.png"
-# end
-# 
-# project_sample "Todo List" do |s|
-  # s.id = "com.appcelerator.titanium.mobile.samples.todo"
-  # s.category = "com.appcelerator.titanium.mobile.samples.category"
-  # s.location = "git://github.com/appcelerator-developer-relations/Sample.Todo.git"
-  # s.description = "A sample tabbed application that creates a basic todo list. With this app you can maintain a listing of tasks to be completed, add to that list, and mark tasks as done."
-  # s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
-  # s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_to_do_list.png"
-# end
+project_sample "Geocoder" do |s|
+  s.id = "com.appcelerator.titanium.mobile.samples.mapping"
+  s.category = "com.appcelerator.titanium.mobile.samples.category"
+  s.location = "git://github.com/appcelerator-developer-relations/Sample.Mapping.git"
+  s.description = "A sample Master/Detail app that uses native maps to plot locations. With it you can forward geocode addresses and add them as annotations to the map."
+  s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
+  s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_geo.png"
+end
+ 
+project_sample "RSS Reader" do |s|
+  s.id = "com.appcelerator.titanium.mobile.samples.rss"
+  s.category = "com.appcelerator.titanium.mobile.samples.category"
+  s.location = "git://github.com/appcelerator-developer-relations/Sample.RSS.git"
+  s.description = "A sample Master/Detail app that creates a RSS reader. With it you can pull live RSS feeds from the internet, list them along with thumbnails, then drill down to the article itself."
+  s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
+  s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_rss.png"
+end
+ 
+project_sample "Todo List" do |s|
+  s.id = "com.appcelerator.titanium.mobile.samples.todo"
+  s.category = "com.appcelerator.titanium.mobile.samples.category"
+  s.location = "git://github.com/appcelerator-developer-relations/Sample.Todo.git"
+  s.description = "A sample tabbed application that creates a basic todo list. With this app you can maintain a listing of tasks to be completed, add to that list, and mark tasks as done."
+  s.natures = ["com.appcelerator.titanium.mobile.nature", "com.aptana.projects.webnature"]
+  s.icon = "http://preview.appcelerator.com/dashboard/img/icons/icon_to_do_list.png"
+end
 
 project_sample "Kitchen Sink" do |s|
   s.id = "com.appcelerator.titanium.mobile.samples.kitchensink"
